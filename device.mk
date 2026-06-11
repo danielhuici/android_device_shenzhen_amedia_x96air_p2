9f40e5bdf0a33c1bca983d90478e65cb338dc54d
danielhuici <danielhuici@hotmail.com>
2026-06-11 16:54:12 +0000

    x96air_p2: Carry libbt-vendor prebuilt for 22.2
    Change-Id: I705edc6060e3c3f4ebcfdaff160e73a0f970e041

--- device.mk
+++ device.mk
@@ -6,8 +6,7 @@
 
 ## Bluetooth
 PRODUCT_PACKAGES += \
-    X96AirP2BluetoothOverlay \
-    libbt-vendor
+    X96AirP2BluetoothOverlay
 
 ## Bluetooth firmware
 include kernel/amlogic/kernel-modules/dhd-driver/firmware/bluetooth/bluetooth.mk
@@ -24,8 +23,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/Vendor_0001_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl \
     $(LOCAL_PATH)/configs/remote/remote-0xfe01.tab:$(TARGET_COPY_OUT_VENDOR)/etc/remote-0xfe01.tab \
     $(LOCAL_PATH)/configs/remote/remote-0x7f80.tab:$(TARGET_COPY_OUT_VENDOR)/etc/remote-0x7f80.tab \
-    $(LOCAL_PATH)/init-files/vfd_av_indicator.sh:$(TARGET_COPY_OUT_VENDOR)/bin/vfd_av_indicator.sh
-
+    $(LOCAL_PATH)/init-files/vfd_av_indicator.sh:$(TARGET_COPY_OUT_VENDOR)/bin/vfd_av_indicator.sh \
+    $(LOCAL_PATH)/blobs/libbt-vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib/libbt-vendor.so
 
 
 ## TEE
